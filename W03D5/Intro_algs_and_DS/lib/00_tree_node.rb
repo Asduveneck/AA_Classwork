019a7a9da1207b177783c0edc1634f37ddf502e9
Alexander Shinerh Duveneck <alexander.duveneck@gmail.com>
2019-09-06 15:25:53 -0600

    Add #parent= and #add_child to PolyTreeNode class

--- W03D5/Intro_algs_and_DS/lib/00_tree_node.rb
+++ W03D5/Intro_algs_and_DS/lib/00_tree_node.rb
@@ -1,23 +1,41 @@
 class PolyTreeNode
-    def initialize(value, children = [], parent = nil)
+  attr_reader :value, :children, :parent
+
+  def initialize(value, children = [], parent = nil)
         @value = value  
         @children = children 
         @parent = parent 
-    end 
+  end 
+
+  def parent=(node) # Never returns anything. .
+    # get the old parent node 
+    old_parent = @parent
+    old_parent.children.delete(self) if old_parent # delete node from old parent 
+    @parent = node # @ for this instance only
+    if node.nil? 
+      @parent = nil 
+      return ;
+    end # handle nil
+    node.children << self unless node.children.include?(self) # add child to 
+     # parent only once
     
-    
+    return; 
+  end 
+
+  def add_child(child)
+    child.parent = self
+  end
 
 end
 
-# Something like this I got notes from today so ... 
+#  describe "#add_child" do
+#     it "should pass itself to the child's #parent=" do
+#       expect(node3).to receive(:parent=).with(node2)
+#       node2.add_child(node3)
+#     end
 
-# Looks good to me; gives us a solid start
-
-class Node
-  attr_reader :value, :children # only done to play around
-
-  def initialize(value, children = [])
-    @value = value
-    @children = children
-  end
-end  
+#     it "doesn't add the child twice" do
+#       node2.add_child(node3)
+#       expect(node2.children).to eq([node3])
+#     end
+#   end
