6e1f9bb1b9eab6736c1eb9657e40bc42b502a122
App Academy Student <student@appacademy.io>
2019-09-17 16:02:43 -0700

    Filled in SQL queries

--- W05D2/SQL_zoo/lib/04_select_within_select.rb
+++ W05D2/SQL_zoo/lib/04_select_within_select.rb
@@ -35,6 +35,19 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries 
+    WHERE
+      population > (
+      SELECT
+        population   
+      FROM
+        countries 
+      WHERE
+        name = 'Russia'
+      )
   SQL
 end
 
@@ -42,6 +55,21 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries 
+    WHERE
+      (gdp/population) > (
+      SELECT
+        gdp/population
+      FROM
+        countries 
+      WHERE
+        name = 'United Kingdom'
+      ) 
+    AND
+     continent = 'Europe'
   SQL
 end
 
@@ -49,6 +77,19 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+    SELECT
+      name, continent
+    FROM
+      countries 
+    WHERE
+      continent IN (
+      SELECT
+        continent
+      FROM
+        countries 
+      WHERE
+        name IN ('Belize', 'Belgium')
+      )
   SQL
 end
 
@@ -56,14 +97,51 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  SELECT
+      name, population
+    FROM
+      countries 
+    WHERE
+      population BETWEEN (
+      SELECT
+        population + 1
+      FROM
+        countries 
+      WHERE
+        name = 'Canada'
+      ) AND (
+      SELECT
+        population - 1
+      FROM
+        countries 
+      WHERE
+        name = 'Poland'
+      )
+      /* Bad people here. Use > and < ... */
   SQL
 end
 
 def sparse_continents
-  # Find every country that belongs to a continent where each country's
-  # population is less than 25,000,000. Show name, continent and
+  # Find every country that belongs to a (continent where each country's
+  # population is less than 25,000,000.) Show name, continent and
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
+  # Suggestion: Rewording problems in the firs tplace would make happier students
   execute(<<-SQL)
+  SELECT
+    name, continent, population
+  FROM
+    countries 
+  WHERE
+    continent IN (
+      SELECT
+        continent 
+      FROM
+        countries
+      GROUP BY
+        continent
+      HAVING 
+        MAX(population) < 25*10^6
+    ) 
   SQL
 end
--- W05D2/SQL_zoo/lib/05_aggregates.rb
+++ W05D2/SQL_zoo/lib/05_aggregates.rb
@@ -22,30 +22,63 @@ end
 def continents
   # List all the continents - just once each.
   execute(<<-SQL)
+  SELECT
+    DISTINCT continent AS continents
+  FROM
+    countries
+  
+
   SQL
 end
 
 def africa_gdp
   # Give the total GDP of Africa.
   execute(<<-SQL)
+  SELECT
+    SUM(gdp) AS africa_gdp
+  FROM
+    countries
+  WHERE
+    continent = 'Africa'
   SQL
 end
 
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+  SELECT
+    COUNT(*) AS num_large_countries
+  FROM
+    countries
+  WHERE
+    area > 10^6
+  
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+  SELECT
+    SUM(population) AS fgs_pop
+  FROM
+    countries
+  WHERE
+    name in ('France', 'Germany', 'Spain')
+  
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+  SELECT
+    continent, COUNT(*) AS num_of_countries
+  FROM
+    countries
+  GROUP BY 
+    continent
+
   SQL
 end
 
@@ -53,11 +86,31 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+  SELECT
+    continent, COUNT(*) AS num_of_countries
+  FROM
+    countries
+  WHERE
+    population >= 10^7
+  GROUP BY
+    continent
+    
+  
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
+
+  SELECT
+    continent
+  FROM 
+    countries
+  GROUP BY
+    continent
+  HAVING
+    SUM(population) >= 10^8
+  
   SQL
 end
