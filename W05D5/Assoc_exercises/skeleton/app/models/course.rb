98566ac14e3dc0356632b16be07fc5eb676f7309
App Academy Student <student@appacademy.io>
2019-09-20 15:14:31 -0700

    Adds basic has_many belongs_to associations for Enrollments. Fails on Enrolled_courses has_many through associations

--- W05D5/Assoc_exercises/skeleton/app/models/course.rb
+++ W05D5/Assoc_exercises/skeleton/app/models/course.rb
@@ -1,2 +1,17 @@
 class Course < ApplicationRecord
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :name,
+    class_name: :User
+
+  has_many :enrollments,
+    primary_key: :id,  
+    foreign_key: :course_id,
+    class_name: :Enrollment 
+
+  has_many :users,
+    through: :courses,
+    source: :Enrollment
+  
 end
--- W05D5/Assoc_exercises/skeleton/app/models/user.rb
+++ W05D5/Assoc_exercises/skeleton/app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ApplicationRecord
+
+  # Validation
+  validates :name, presence: true
+
+  # Associations:
+
+  # belongs_to :enrolled_courses,
+  #   primary_key: :id, 
+  #   foreign_key: :SOMETHING!, # THROUGH SOMETHING?!?
+  #   class_name: :Course
+
+
+    # From Enrollment
+    has_many :enrollments,
+    primary_key: :id,  
+    foreign_key: :student_id,
+    class_name: :Enrollment
+
+    has_many :courses,
+      primary_key: :id,
+      foreign_key: :name,
+      class_name: :User
+
 end
