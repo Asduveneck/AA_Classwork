d3f367ba6ae23cdc6c7c179cc138be5181230db9
Alexander Shinerh Duveneck <alexander.duveneck@gmail.com>
2019-09-20 15:14:31 -0700

    Adds basic has_many belongs_to associations for Enrollments. Fails on Enrolled_courses has_many through associations

--- W05D5/Assoc_exercises/skeleton/app/models/enrollment.rb
+++ W05D5/Assoc_exercises/skeleton/app/models/enrollment.rb
@@ -1,2 +1,31 @@
 class Enrollment < ApplicationRecord
+  # validation: 
+  validates :course_id, :student_id, presence: true
+
+  # Associations
+  # belongs_to
+
+  belongs_to :enrolled_courses,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
+  
+  belongs_to :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
+
+    
+    # has_many
+  has_many :users,
+    primary_key: :id,
+    foreign_key: :name,
+    class_name: :User
+  
+
 end
