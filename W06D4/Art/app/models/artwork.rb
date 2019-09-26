2cb30cb6129a5aed9a36aaca1d3537fa5e9e9ad3
Alexander Shinerh Duveneck <alexander.duveneck@gmail.com>
2019-09-26 10:12:28 -0700

    Other Alex is amazing. Unlike this one haha

---
+++ W06D4/Art/app/models/artwork.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  image_url  :string           not null
+#  artist_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artwork < ApplicationRecord
+    validates :title, :image_url, presence: true
+    validates :image_url, uniqueness: true
+    validates :title, uniqueness: { scope: :artist_id }
+
+    has_many :artwork_shares,
+        foreign_key: :artwork_id,
+        class_name: :ArtworkShare
+end
