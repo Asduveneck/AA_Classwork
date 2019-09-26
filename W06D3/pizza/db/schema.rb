ca40de2b2762827185e388d06b7d2e9f53840308
App Academy Student <student@appacademy.io>
2019-09-25 17:59:14 -0700

    Working on creating classes and modifying them with new migrations

--- W06D3/pizza/db/schema.rb
+++ W06D3/pizza/db/schema.rb
@@ -10,14 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_222928) do
+ActiveRecord::Schema.define(version: 2019_09_26_004018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "artwork_shares", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "artwork_id", null: false
+    t.integer "viewer_id", null: false
+  end
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image_url", null: false
+    t.integer "artist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
   end
 
 end
