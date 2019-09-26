1aabee7c96efabfc2b5a98cfddde60ea1a0ed3ed
App Academy Student <student@appacademy.io>
2019-09-26 10:12:28 -0700

    Other Alex is amazing. Unlike this one haha

---
+++ W06D4/Art/app/models/artwork_share.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: artwork_shares
+#
+#  id         :bigint           not null, primary key
+#  artwork_id :integer          not null
+#  viewer_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class ArtworkShare < ApplicationRecord
+    validates :artwork_id, uniqueness: {scope: :viewer_id}
+    validates :favorite, inclusion: {in: [true, false]}
+
+    belongs_to :artwork,
+        foreign_key: :artwork_id,
+        class_name: :Artwork
+
+    belongs_to :viewer,
+        foreign_key: :user_id,
+        class_name: :User
+end
---
+++ W06D4/Art/test/models/user_test.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
