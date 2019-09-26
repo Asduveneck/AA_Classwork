eef4684706f55fd350f2dee3e2471be94ba1de8b
Alexander Duveneck <alexander.duveneck@gmail.com>
2019-09-25 17:59:14 -0700

    Working on creating classes and modifying them with new migrations

--- W06D3/pizza/app/models/user.rb
+++ W06D3/pizza/app/models/user.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  username   :string           not null
+#
+
 class User < ApplicationRecord
+  # for built in
+  validates :username, presence: true, uniqueness: true 
+  # validate # custom methods we define in the model
 end
