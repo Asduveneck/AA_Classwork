e1d3efa83a2019f471f2f2fcb53cfaf0fdfdca90
App Academy Student <student@appacademy.io>
2019-10-02 16:54:53 -0700

    work errors on controllers spec

--- W07D3/goalSetter/app/controllers/sessions_controller.rb
+++ W07D3/goalSetter/app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < ApplicationController
+
   # Auth methods go here
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+    if user
+      login!(user)
+      user.id = current_user.id
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = ["Invalid Credentials"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout!
+  end
+
 end
--- W07D3/goalSetter/spec/rails_helper.rb
+++ W07D3/goalSetter/spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'shoulda-matchers'
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -56,6 +59,14 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+  
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
