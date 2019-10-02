06f5c0b180e03676cdc7267c5c7d2bea11cf577b
Alexander Duveneck <alexander.duveneck@gmail.com>
2019-10-02 14:20:47 -0700

    First rspecs

--- W07D3/goalSetter/db/migrate/20191002210238_create_users.rb
+++ W07D3/goalSetter/db/migrate/20191002210238_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username, null: false
-      t.string :email, null: false
-      t.string :password_digest, null: false
-      t.string :session_token, null: false
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :session_token
 
       t.timestamps
     end
--- W07D3/goalSetter/db/schema.rb
+++ W07D3/goalSetter/db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_203738) do
+ActiveRecord::Schema.define(version: 2019_10_02_210238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "email", null: false
-    t.string "password_digest", null: false
-    t.string "session_token", null: false
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "session_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
