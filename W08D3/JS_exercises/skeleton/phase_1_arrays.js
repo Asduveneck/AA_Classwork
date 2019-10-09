11c54e9fbcacadd91889b8279bb20f3ed7ee2049
Alexander Shinerh Duveneck <alexander.duveneck@gmail.com>
2019-10-09 16:59:08 -0700

    Finishes phases 1-2 of JS Exercises

--- W08D3/JS_exercises/skeleton/phase_1_arrays.js
+++ W08D3/JS_exercises/skeleton/phase_1_arrays.js
@@ -1,4 +1,10 @@
 let arr = [1, 2, 3, 2, 3, 4, 5, 4, 7, 5];
+let arr2 = [ 1, 3, -2, -3, 3, 2, -1, 1, 2, 4 ];
+let matrix = [
+  [11, 12, 13],
+  [21, 22, 23],
+  [31, 32, 33]
+];
 
 // function uniq_arr(arr){
 //   let new_arr = [];
@@ -10,8 +16,6 @@ let arr = [1, 2, 3, 2, 3, 4, 5, 4, 7, 5];
 //   return new_arr;
 // }
 
-
-
 Array.prototype.uniq = function() {
   //console.log(this);
   let new_arr = [];
@@ -21,9 +25,67 @@ Array.prototype.uniq = function() {
     }
   });
   return new_arr;
-}
+};
 
 
 
 // array.forEach(element => {
 // }
+
+/*
+  Multi line comment
+
+  Two Sum: return an array of position pairs where elements sum to zero!
+*/
+
+Array.prototype.twosum = function() { 
+  let arr_dup = this // is this good js?
+  let new_arr = [];
+
+  this.forEach(function (num1, idx1) {
+    // console.log(num1);
+    // console.log(idx1);
+
+    arr_dup.forEach(function (num2, idx2) {
+      //console.log(num2);
+      // console.log(idx2);
+      if(idx2>idx1) {
+        if(num2+num1 === 0) {
+          new_arr.push([idx1, idx2]);
+        }
+      }
+    });  
+  });
+
+  return new_arr;
+};
+
+Array.prototype.transpose = function() {
+  let trans = [];
+  let arr_dup = this;
+  let arr_dup2 = this;
+  arr_dup.forEach(function (row, idx1) { // each row
+    let new_r = [];
+    row.forEach(function (ele, idx2){
+      // arr_dup2[idx1][idx2] = arr_dup[idx2][idx1];
+      new_r.push(arr_dup[idx2][idx1]);
+    });
+      trans.push(new_r);
+  });
+  //return arr_dup2;
+  return trans;
+};
+
+
+/*
+
+[11][12][13]
+[21][22][23]
+[31][32][33]
+==========
+[11][21][31]
+[12][22][32]
+[13][23][33]
+
+*/
+
