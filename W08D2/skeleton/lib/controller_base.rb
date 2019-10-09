3e5ecc6ca060bd99755c82c91c8239ef69eb0811
App Academy Student <student@appacademy.io>
2019-10-08 17:01:29 -0700

    more attempts to work on updating instance variable

--- W08D2/skeleton/lib/controller_base.rb
+++ W08D2/skeleton/lib/controller_base.rb
@@ -10,12 +10,16 @@ class ControllerBase
   def initialize(req, res)
     @req = req 
     @res = res
-    @already_built_response = false
+    @already_built_response = false #= nil # false # implied
   end
 
   # Helper method to alias @already_built_response
   def already_built_response? # just a getter. ? because bool, and also because we're getting it...
     @already_built_response 
+  end
+
+  def already_built_response=(val)
+    @already_built_response = val
   end
 
   # Set the response status code and header
@@ -30,15 +34,21 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    # Raise error first 
+
+    raise 'Double Render Error' if @already_built_response # == true   # check truthyness
+    
     # set the content+type and body:
-    raise 'Double Render Error' if @already_built_response == true  
     
     #@content_type = content_type # no because our res is of class Rack. To access/modify one of its parameters, content, we invoke the `write` method...
     @res.write(content)
     @res['Content-Type'] = content_type # in this response instance, have this key of content type = what was passed  in... already a string
-  
-    @already_built_response = true # because now we've built the response...
-    debugger
+
+    # built response:
+
+    # self.sresponse= true also didn't work
+    @already_built_response=(true) # because now we've built the response...
+    # debugger # declare a setter via: https://stackoverflow.com/questions/51997432/how-can-i-update-the-value-of-an-instance-variable
   end
 
   # use ERB and binding to evaluate templates
