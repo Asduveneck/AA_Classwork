43bdd31e1da74b00161ab8ef26ecd80d76f1de17
App Academy Student <student@appacademy.io>
2019-10-11 16:57:14 -0700

    Initializes last part

---
+++ W08D5/Intro_Callback/p03_function_calls.js
@@ -0,0 +1,13 @@
+function myBind(context) {
+
+} 
+/*
+// We want to add this to the function prototype. 
+// Return an arrow function, capturing this and context. 
+
+1. Return an arrow function.
+2. The arrow function captures this and context.
+3. In the anonymous function, call Function.prototype.apply on this, passing the context.
+
+*/
+
